fd3dfae3989ed529516720a8e2ec200ded9b8f97
NadavSklar <sklar@post.bgu.ac.il>
2018-12-26 12:58:27 +0200

    Create Views.sql
    views

---
+++ Views.sql
@@ -0,0 +1,25 @@
+USE DB2019_Ass2
+GO
+
+CREATE VIEW ConstructionEmployeeOverFifty
+AS
+	SELECT Employee.*, ConstructorEmployee.CompanyName, ConstructorEmployee.SalaryPerDay
+	FROM Employee, ConstructorEmployee
+	WHERE Employee.EID = ConstructorEmployee.EID AND DATEDIFF(YEAR, Employee.BirthDate, GETDATE()) >= 50
+GO
+
+CREATE VIEW ApartmentNumberInNeighborhood
+AS
+	SELECT Neighborhood.[Name], Count(*) AS ApartmentsCount
+	FROM Neighborhood JOIN Apartment ON Apartment.[NeighborhoodID] = Neighborhood.[NID]
+	GROUP BY [NAME], [NID]
+GO
+
+CREATE VIEW MaxParking
+AS
+	SELECT CarParking.[ParkingAreaID], CarParking.[CID], COUNT(*) AS NumberOfParkings
+	FROM CarParking JOIN ParkingArea ON CarParking.[ParkingAreaID] = ParkingArea.[AID]
+	GROUP BY [ParkingAreaID],[CID]
+	HAVING COUNT(CID) >= ALL (SELECT COUNT(CID) FROM CarParking WHERE CarParking.[ParkingAreaID] = ParkingAreaID AND CarParking.[CID] = CID GROUP BY CID)
+	ORDER BY NumberOfParkings DESC  
+GO
