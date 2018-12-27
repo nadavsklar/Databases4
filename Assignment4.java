8a6180f898fbf0b78a1c265de288481fcb985203
NadavSklar <sklar@post.bgu.ac.il>
2018-12-27 22:39:44 +0200

    uploading changes
    changes

--- Assignment4.java
+++ Assignment4.java
@@ -5,6 +5,7 @@ import java.sql.*;
 import java.util.Calendar;
 import java.text.SimpleDateFormat;
 import java.io.File;
+import java.io.*;
 
 import javafx.util.Pair;
 
@@ -14,11 +15,13 @@ import java.util.ArrayList;
 
 public class Assignment4 {
 
+    private final String databaseName = "[DB2019_Ass2]";
+    private final DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
 
     private Assignment4() {
     }
 
-   public static void executeFunc(Assignment4 ass, String[] args) {
+    public static void executeFunc(Assignment4 ass, String[] args) {
         String funcName = args[0];
         switch (funcName) {
             case "loadNeighborhoodsFromCsv":
@@ -68,10 +71,21 @@ public class Assignment4 {
     public static void main(String[] args) {
 
         File file = new File(".");
-        String csvFile = args[0];
+        String csvFile = "src/filename.csv";
         String line = "";
         String cvsSplitBy = ",";
         Assignment4 ass = new Assignment4();
+
+        ass.updateAllProjectsBudget(100);
+        System.out.println(ass.getEmployeeTotalSalary());
+//        System.out.println(ass.getTotalProjectBudget());
+//        System.out.println(ass.calculateIncomeFromParking(2018));
+//        ass.AddEmployee(43242,"Levi","Jonathan",new Date(1),
+//                "ADMONIT",8,6,"RAANANA");
+        System.out.println(ass.getMostProfitableParkingAreas());
+        System.out.println(ass.getNumberOfParkingByArea());
+        System.out.println(ass.getNumberOfDistinctCarsByArea());
+
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
             while ((line = br.readLine()) != null) {
@@ -86,59 +100,209 @@ public class Assignment4 {
         } catch (IOException e) {
             e.printStackTrace();
 
-        } 
+        }
     }
 
 
 
     private void loadNeighborhoodsFromCsv(String csvPath) {
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
 
+        try {
+            br = new BufferedReader(new FileReader(csvPath));
+            while ((line = br.readLine()) != null) {
+                String[] row = line.split(cvsSplitBy);
+                DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+                databaseManager.startConnection();
+                int id = Integer.parseInt(row[0]);
+                String name = "'" + row[1] + "'";
+                databaseManager.executeQueryVoid("INSERT INTO Neighborhood VALUES(" + id + "," + name + ")");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        databaseManager.closeConnection();
     }
 
     private void updateEmployeeSalaries(double percentage) {
-
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+        databaseManager.executeQueryVoid("UPDATE ConstructorEmployee\n" +
+                "SET SalaryPerDay = SalaryPerDay * " + (1 + (percentage/100)) + "\n" +
+                "WHERE EID IN (SELECT ConstructorEmployee.EID\n" +
+                "\tFROM Employee, ConstructorEmployee\n" +
+                "\tWHERE Employee.EID = ConstructorEmployee.EID AND DATEDIFF(YEAR, Employee.BirthDate, GETDATE()) >= 50)");
+        databaseManager.closeConnection();
     }
 
 
     public void updateAllProjectsBudget(double percentage) {
-
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+        databaseManager.executeQueryVoid("UPDATE Project\n" +
+                "SET Budget = Budget * " + (1 + (percentage / 100)));
+        databaseManager.closeConnection();
     }
 
 
     private double getEmployeeTotalSalary() {
-
+        double result = 0;
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+        ResultSet resultSet = databaseManager.executeQuerySelect("SELECT SUM(SalaryPerDay) as Total\n" +
+                "FROM ConstructorEmployee");
+        try {
+            resultSet.next();
+            result = resultSet.getDouble(1);
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        databaseManager.closeConnection();
+        return result;
     }
 
 
     private int getTotalProjectBudget() {
-
+        int result = 0;
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+        ResultSet resultSet = databaseManager.executeQuerySelect("SELECT SUM(Budget)\n" +
+                "FROM Project");
+        result = getResult(result, resultSet);
+        databaseManager.closeConnection();
+        return result;
     }
     private void dropDB() {
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+
+        String query = "DROP DATABASE DB2019_Ass2";
+
+        databaseManager.executeQueryVoid(query);
+        databaseManager.closeConnection();
 
     }
 
     private void initDB(String csvPath) {
-
+        //ToDO - this
     }
     private int calculateIncomeFromParking(int year) {
+        int result = 0;
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+        ResultSet resultSet = databaseManager.executeQuerySelect("SELECT SUM(COST)\n" +
+                "FROM CarParking\n" +
+                "WHERE YEAR(StartTime) = " + year);
+        result = getResult(result, resultSet);
+        databaseManager.closeConnection();
+        return result;
+    }
 
+    private int getResult(int result, ResultSet resultSet) {
+        try {
+            resultSet.next();
+            result = resultSet.getInt(1);
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
     private ArrayList<Pair<Integer, Integer>> getMostProfitableParkingAreas() {
+        ArrayList<Pair<Integer,Integer>> mostProfitable = new ArrayList<>();
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
 
+        String query = "SELECT Top 5 AID, SUM(CarParking.Cost) as TotalProfit\n" +
+                "FROM ParkingArea LEFT JOIN CarParking ON ParkingArea.[AID] = CarParking.[ParkingAreaID]\n" +
+                "GROUP BY AID\n" +
+                "ORDER BY TotalProfit DESC";
+
+        ResultSet resultSet = databaseManager.executeQuerySelect(query);
+        try {
+            while(resultSet.next()){
+                mostProfitable.add(
+                        new Pair(resultSet.getInt(1),resultSet.getInt(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return mostProfitable;
     }
 
     private ArrayList<Pair<Integer, Integer>> getNumberOfParkingByArea() {
 
+        ArrayList<Pair<Integer,Integer>> numOfParkings = new ArrayList<>();
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+
+        String query = "SELECT AID, COUNT(CarParking.ParkingAreaID) as NumOfParkings\n" +
+                "FROM ParkingArea LEFT JOIN CarParking ON CarParking.ParkingAreaID = ParkingArea.AID\n" +
+                "GROUP BY AID";
+
+        ResultSet resultSet = databaseManager.executeQuerySelect(query);
+        try {
+            while(resultSet.next()){
+                numOfParkings.add(
+                        new Pair(resultSet.getInt(1),resultSet.getInt(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+        return numOfParkings;
     }
 
 
     private ArrayList<Pair<Integer, Integer>> getNumberOfDistinctCarsByArea() {
+        ArrayList<Pair<Integer,Integer>> numOfCars = new ArrayList<>();
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
 
+        String query = "SELECT AID, COUNT(DISTINCT CarParking.CID) as NumOfCars\n" +
+                "FROM ParkingArea LEFT JOIN CarParking ON ParkingArea.AID = CarParking.ParkingAreaID\n" +
+                "GROUP BY AID";
+
+        ResultSet resultSet = databaseManager.executeQuerySelect(query);
+        try {
+            while(resultSet.next()){
+                numOfCars.add(
+                        new Pair(resultSet.getInt(1),resultSet.getInt(2)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return numOfCars;
     }
 
+    private void AddEmployee(int EID, String LastName, String FirstName, Date BirthDate,
+                             String StreetName, int Number, int door, String City) {
+        DatabaseManager databaseManager = new DatabaseManagerMSSQLServer(databaseName);
+        databaseManager.startConnection();
+        String query =
+        "INSERT INTO [Employee]\n" +
+                "\tVALUES\n" +
+                "\t(" + EID + ",'" + LastName + "','" + FirstName +"','" + BirthDate +
+                "','" + StreetName + "'," +Number + "," + door + ",'" + City + "')";
 
-    private void AddEmployee(int EID, String LastName, String FirstName, Date BirthDate, String StreetName, int Number, int door, String City) {
-
+        databaseManager.executeQueryVoid(query);
+        databaseManager.closeConnection();
     }
 }
--- Triggers.sql
+++ Triggers.sql
@@ -14,21 +14,59 @@ GO
 CREATE TRIGGER Park ON CarParking
 FOR UPDATE
 AS
-	DECLARE @CarID int
-	SET @CarID = (SELECT CarParking1.[CID] FROM inserted CarParking1 JOIN deleted CarParking2 ON (CarParking1.CID = CarParking2.CID AND CarParking1.StartTime = CarParking2.StartTime))
-	DECLARE @ParkingAreaID int
-	SET @ParkingAreaID = (SELECT TOP 1 CarParking.ParkingAreaID FROM CarParking WHERE @CarID = CarParking.[CID])
-	DECLARE @RealCost int
-	DECLARE @StartTime DATETIME
-	DECLARE @EndTIme DATETIME
-	SET @StartTime = (SELECT CarParking1.[StartTime] FROM inserted CarParking1 JOIN deleted CarParking2 ON (CarParking1.CID = CarParking2.CID AND CarParking1.StartTime = CarParking2.StartTime))
-	SET @EndTIme = (SELECT CarParking1.[EndTime] FROM inserted CarParking1 JOIN deleted CarParking2 ON (CarParking1.CID = CarParking2.CID AND CarParking1.StartTime = CarParking2.StartTime))
-	SET @RealCost = (DATEDIFF(HOUR, @StartTime, @EndTime) * (SELECT TOP 1 priceperhour FROM ParkingArea WHERE AID = @ParkingAreaID))
-	DECLARE @MaxPrice int
-	SET @MaxPrice = (SELECT maxpriceperday FROM ParkingArea WHERE AID = @ParkingAreaID)
-	UPDATE CarParking
-	SET EndTime = @EndTime,
-		Cost = (SELECT Case When @RealCost < @MaxPrice
-							Then @RealCost Else @MaxPrice END)
-	WHERE CID = @CarID AND StartTime = @StartTime
+	IF TRIGGER_NESTLEVEL() > 1
+	BEGIN
+		RETURN
+    END
+	IF UPDATE(EndTime)
+	BEGIN
+		DECLARE @CarID int
+		SET @CarID = (SELECT CarParking1.[CID] FROM inserted CarParking1 JOIN deleted CarParking2 ON (CarParking1.CID = CarParking2.CID AND CarParking1.StartTime = CarParking2.StartTime))
+		DECLARE @ParkingAreaID int
+		SET @ParkingAreaID = (SELECT TOP 1 CarParking.ParkingAreaID FROM CarParking WHERE @CarID = CarParking.[CID])
+		DECLARE @RealCost int
+		DECLARE @StartTime DATETIME
+		DECLARE @EndTIme DATETIME
+		SET @StartTime = (SELECT CarParking1.[StartTime] FROM inserted CarParking1 JOIN deleted CarParking2 ON (CarParking1.CID = CarParking2.CID AND CarParking1.StartTime = CarParking2.StartTime))
+		SET @EndTIme = (SELECT CarParking1.[EndTime] FROM inserted CarParking1 JOIN deleted CarParking2 ON (CarParking1.CID = CarParking2.CID AND CarParking1.StartTime = CarParking2.StartTime))
+		SET @RealCost = (DATEDIFF(HOUR, @StartTime, @EndTime) * (SELECT TOP 1 priceperhour FROM ParkingArea WHERE AID = @ParkingAreaID))
+		DECLARE @MaxPrice int
+		SET @MaxPrice = (SELECT maxpriceperday FROM ParkingArea WHERE AID = @ParkingAreaID)
+		UPDATE CarParking
+		SET EndTime = @EndTime,
+			Cost = (SELECT Case When @RealCost < @MaxPrice
+								Then @RealCost Else @MaxPrice END)
+		WHERE CID = @CarID AND StartTime = @StartTime
+	END
+GO
+
+CREATE TRIGGER ParkingDiscount ON CarParking
+FOR UPDATE
+AS
+	IF TRIGGER_NESTLEVEL() > 2
+	BEGIN
+		RETURN
+	END
+	IF UPDATE(Cost)
+	BEGIN
+		DECLARE @CarId int
+		SET @CarId = (SELECT CarParking.[CID] FROM inserted CarParking)
+		DECLARE @StartTime DATETIME
+		SET @StartTime = (SELECT CarParking.[StartTime] FROM inserted CarParking)
+		DECLARE @Id int
+		SET @Id = (SELECT Cars.[ID] FROM Cars WHERE Cars.[CID] = @CarId)
+		DECLARE @RealCost int
+		SET @RealCost = (SELECT CarParking.[Cost] FROM inserted CarParking)
+		DECLARE @DiscountCost int
+		SET @DiscountCost = 0.8 * @RealCost
+		PRINT @RealCost
+		PRINT @DiscountCost
+		IF EXISTS (SELECT 1 FROM Employee WHERE Employee.[EID] = @Id)
+		BEGIN
+			SET @RealCost = @DiscountCost
+		END
+		UPDATE CarParking
+		SET Cost = @RealCost
+		WHERE CID = @CarId AND StartTime = @StartTime
+	END
 GO
